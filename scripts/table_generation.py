2c4c6082c51494ce1371128ce4acb839771d1378
Feyi Adesanya <feyi.adesanya176@gmail.com>
2025-04-11 13:50:46 -0400

    generalize id names

--- scripts/table_generation.py
+++ scripts/table_generation.py
@@ -273,16 +273,16 @@ class Analysis:
 # RQ 1 
 # =======================          
     def motivationsTable(self):
-        self.generate_summary_table("Motivation (Clustered)", "Motivations in Studies", "motivations", "p{2.5cm} l p{15cm}", "Motivation", "RQ1/motivations")
+        self.generate_summary_table("Motivation (Clustered)", "Motivations in Studies", "motivations-table", "p{2.5cm} l p{15cm}", "Motivation", "RQ1/motivations")
         
     def intentsTable(self):
-        self.generate_summary_table("Intent", "Intents in Studies", "rq1-intent", "p{4cm} l p{13.5cm}", "Intent", "RQ1/intentsTable")
+        self.generate_summary_table("Intent", "Intents in Studies", "intents-table", "p{4cm} l p{13.5cm}", "Intent", "RQ1/intentsTable")
         
     def domainsTable(self):
         self.generate_other_cat_table(
             group_by_col="Domain (Aggregated)",
             latex_caption="Domains of Studies",
-            latex_label="rq1-domains",
+            latex_label="domains-table",
             latex_tabular_size="p{4cm} l p{13.5cm}",
             latex_first_column="Domain",
             latex_filename="RQ1/domainsTable",
@@ -321,7 +321,7 @@ class Analysis:
             "\\centering",
             "\\setlength{\\tabcolsep}{1em}",
             "\\caption{Challenges in studies}",
-            "\\label{tab:rq1-challenges}",
+            "\\label{tab:challenges-table}",
             "\\footnotesize",
             "\\begin{tabular}{@{}p{5.0cm} l p{8cm}@{}}", 
             "\\toprule",
@@ -361,16 +361,16 @@ class Analysis:
 # RQ 2
 # =======================
     def topologyExtractionTable(self):        
-        self.generate_summary_table("Topology of DT/PT (Cleaned)", "Topologies in Studies", "rq2-topology", "p{2.5cm} l p{15cm}", "Topology", "RQ2/topologyExtractionTable")
+        self.generate_summary_table("Topology of DT/PT (Cleaned)", "Topologies in Studies", "topology-table", "p{2.5cm} l p{15cm}", "Topology", "RQ2/topologyExtractionTable")
 
     def spatialDistributionTable(self):
-        self.generate_summary_table("Spatial Distribution", "Spatially Distributed Topologies in Studies", "rq2-spatial-distribution", "p{3.5cm} l p{15cm}", "Distribution", "RQ2/spatialDistributionTable")
+        self.generate_summary_table("Spatial Distribution", "Spatially Distributed Topologies in Studies", "spatial-distribution-table", "p{3.5cm} l p{15cm}", "Distribution", "RQ2/spatialDistributionTable")
         
     def coordinationExtractionTable(self):
-        self.generate_summary_table("Coordination (Cleaned)", "Coordination in Studies", "rq2-coordination", "p{3.5cm} l p{15cm}", "Coordination", "RQ2/coordinationExtractionTable")
+        self.generate_summary_table("Coordination (Cleaned)", "Coordination in Studies", "coordination-table", "p{3.5cm} l p{15cm}", "Coordination", "RQ2/coordinationExtractionTable")
     
     def constituentUnitsTable(self):
-        self.generate_summary_table("Constituent unit (higher level aggregation)", "Constituent Units in Studies", "rq2-constituent-units", "p{5cm} l p{12.5cm}", "Constituent Unit", "RQ2/constituentUnitsTable")
+        self.generate_summary_table("Constituent unit (higher level aggregation)", "Constituent Units in Studies", "constituent-units-table", "p{5cm} l p{12.5cm}", "Constituent Unit", "RQ2/constituentUnitsTable")
         
     def programmingLangaugesTables(self, threshold=0):
         self.generate_hierarchical_table(
@@ -380,7 +380,7 @@ class Analysis:
                 "Data Representation"
             ],
             caption="Programming Langauges and Data Formats Methods Used in Studies",
-            label="programming-languages-structured",
+            label="programming-languages-structured-table",
             filename="RQ2/hierarchicalProgrammingLanguagesTable",
             column_label="Category",
             threshold=threshold
@@ -396,7 +396,7 @@ class Analysis:
                 "Application Development & Web Technologies"
             ],
             caption="Tools and Frameworks Used in Studies",
-            label="frameworks-structured",
+            label="frameworks-structured-table",
             filename="RQ2/hierarchicalFrameworksTable",
             column_label="Category",
             threshold=threshold,
@@ -417,13 +417,13 @@ class Analysis:
 # RQ 3 
 # =======================
     def autonomyTable(self):
-        self.generate_summary_table("DT Class", "Levels of Autonomy in Studies", "rq3-autonomy", "p{5cm} l p{13.5cm}", "Autonomy", "RQ3/autonomyTable")
+        self.generate_summary_table("DT Class", "Levels of Autonomy in Studies", "autonomy-table", "p{5cm} l p{13.5cm}", "Autonomy", "RQ3/autonomyTable")
 
     def dtServicesTable(self):
         self.generate_delimiter_table(
         column="Services (Cleaned)", 
         caption="DT Services Used in Papers", 
-        label="rq3-dt-services", 
+        label="dt-services-table", 
         tabular_size="p{3.5cm} l p{14cm}", 
         first_column_name="Service", 
         save_location="RQ3/dtServicesTable"
@@ -444,7 +444,7 @@ class Analysis:
                 "AI and Machine Learning"
             ],
             caption="Modeling and Simulation Methods Used in Studies",
-            label="modeling-methods-structured",
+            label="modeling-methods-structured-table",
             filename="RQ3/hierarchicalModelingMethodsTable",
             column_label="Category",
             threshold=threshold
@@ -456,10 +456,10 @@ class Analysis:
 # RQ 4 
 # =======================   
     def sosTypeTable(self):
-            self.generate_summary_table("Type of SoS", "SoS Type in Studies", "sos-type", "p{2.5cm} l p{14cm}", "SoS", "RQ4/sosTypeTable")
+            self.generate_summary_table("Type of SoS", "SoS Type in Studies", "sos-type-table", "p{2.5cm} l p{14cm}", "SoS", "RQ4/sosTypeTable")
 
     def emergenceTable(self):
-        self.generate_summary_table("Emergence", "Emergence Type in Studies", "emergence-type", "p{1.5cm} l p{15cm}", "Emergence", "RQ4/emergenceTable")
+        self.generate_summary_table("Emergence", "Emergence Type in Studies", "emergence-type-table", "p{1.5cm} l p{15cm}", "Emergence", "RQ4/emergenceTable")
             
 # =======================
 # RQ 5 
@@ -472,7 +472,7 @@ class Analysis:
             "Deployed Prototype",
             "Operational"
         ]
-        self.generate_summary_table("TRL", "TRL in Studies", "trl", "p{3.5cm} l p{15cm}", "TRL", "RQ5/trlTable", custom_order)            
+        self.generate_summary_table("TRL", "TRL in Studies", "trl-table", "p{3.5cm} l p{15cm}", "TRL", "RQ5/trlTable", custom_order)            
             
     def generate_structured_eval_table(self):
         df = self.df.copy()
@@ -505,7 +505,7 @@ class Analysis:
             "\\centering",
             "\\setlength{\\tabcolsep}{1em}",
             "\\caption{Evaluation types and methods used in studies}",
-            "\\label{tab:rq5-evaluation-structured}",
+            "\\label{tab:evaluation-structured-table}",
             "\\footnotesize",
             "\\begin{tabular}{@{}p{4.0cm} l p{10cm}@{}}", 
             "\\toprule",
@@ -535,7 +535,7 @@ class Analysis:
 
         
     def contributionTypeTable(self):
-        self.generate_summary_table("Contribution type", "Contribution Type in Studies", "rq5-contribution-type", "p{2cm} l p{15.5cm}", "Contribution", "RQ5/contributionTypeTable")
+        self.generate_summary_table("Contribution type", "Contribution Type in Studies", "contribution-type-table", "p{2cm} l p{15.5cm}", "Contribution", "RQ5/contributionTypeTable")
                   
     def standardsTable(self, threshold=2):
         df = self.df.copy()
@@ -574,11 +574,11 @@ class Analysis:
                 }])
             ], ignore_index=True)
         
-        latex_table = self.generate_latex_table(summary_df, "Standards Used in Studies", "standards", "p{6.5cm} l p{10cm}", "Standard")
+        latex_table = self.generate_latex_table(summary_df, "Standards Used in Studies", "standards-table", "p{6.5cm} l p{10cm}", "Standard")
         self.saveLatex("RQ5/standards", latex_table)
         
     def dtOrSoSRelated(self):
-        self.generate_summary_table("Do The Studies Use Standards in More of an SoS or DT context", "Context of Standards used in Studies", "dtOrSoSRelated", "p{2cm} l p{15.5cm}", "Context", "RQ5/dtOrSoSRelated")
+        self.generate_summary_table("Do The Studies Use Standards in More of an SoS or DT context", "Context of Standards used in Studies", "dt-or-sos-related-table", "p{2cm} l p{15.5cm}", "Context", "RQ5/dtOrSoSRelated")
         
 
     
@@ -593,7 +593,7 @@ class Analysis:
             "Explicitly Modelled",
             "Evaluated or Validated"
         ]
-        self.generate_summary_table("Security/Confidentiality Level", "Security in Studies", "security", "p{4cm} l p{13.5cm}", "Context", "RQ7/securityTable", custom_order)
+        self.generate_summary_table("Security/Confidentiality Level", "Security in Studies", "security-table", "p{4cm} l p{13.5cm}", "Context", "RQ7/securityTable", custom_order)
         
     def reliabilityTable(self):
         custom_order = [
@@ -603,7 +603,7 @@ class Analysis:
             "Explicitly Modelled",
             "Evaluated or Validated"
         ]
-        self.generate_summary_table("Reliability Level", "Reliability in Studies", "reliability", "p{4cm} l p{13.5cm}", "Context", "RQ7/reliabilityTable", custom_order)
+        self.generate_summary_table("Reliability Level", "Reliability in Studies", "reliability-table", "p{4cm} l p{13.5cm}", "Context", "RQ7/reliabilityTable", custom_order)
 
                      
 # =======================
